bef47f587befe8bfd1481a5aa0939e5fa0bacac1
hoka.do <hoka.do@PCT201908-054>
2024-12-19 13:41:55 +0700

    Add project files.

---
+++ TW/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TW.DATA;
+
+namespace TW.Controllers
+{
+    public abstract class BaseController<T> : Controller where T : BaseController<T>
+    {
+        private GeneralContext db;
+        protected GeneralContext _db => db ??= HttpContext?.RequestServices.GetService<GeneralContext>();
+    }
+}
